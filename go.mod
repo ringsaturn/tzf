30c0d985f673aa6ff9744484a470a60ee2ce0a1c
ringsaturn <ringsaturn.me@gmail.com>
2022-05-29 20:30:31 +0800

    add benchmark and example

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/mitchellh/mapstructure v1.5.0
+	github.com/ringsaturn/tzf-rel v0.0.0-20220529122343-0d9d5887ea4f
 	github.com/tidwall/geometry v0.1.0
 	google.golang.org/protobuf v1.28.0
 )
---
+++ tzf_test.go
@@ -0,0 +1,33 @@
+package tzf_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ringsaturn/tzf"
+	tzfrel "github.com/ringsaturn/tzf-rel"
+	"github.com/ringsaturn/tzf/pb"
+	"google.golang.org/protobuf/proto"
+)
+
+var f *tzf.Finder
+
+func init() {
+	input := &pb.Timezones{}
+	if err := proto.Unmarshal(tzfrel.LiteData, input); err != nil {
+		panic(err)
+	}
+	finder, _ := tzf.NewFinderFromPB(input)
+	f = finder
+}
+
+func BenchmarkGetTimezoneName(b *testing.B) {
+	for i := 0; i <= b.N; i++ {
+		_ = f.GetTimezoneName(116.6386, 40.0786)
+	}
+}
+
+func ExampleFinder_GetTimezoneName() {
+	fmt.Println(f.GetTimezoneName(116.6386, 40.0786))
+	// Output: Asia/Shanghai
+}
