2ced49e1bca1b53e6c53c16e947ca84ac213e685
github-action-benchmark <github@users.noreply.github.com>
2022-07-16 08:23:54 +0000

    add Go Benchmark (go) benchmark result for 1f82feefa9540e47b111496f02a7a5c45a92529e

---
+++ docs/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1657959834842,
+  "repoUrl": "https://github.com/ringsaturn/tzf",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "committer": {
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "id": "1f82feefa9540e47b111496f02a7a5c45a92529e",
+          "message": "add cb",
+          "timestamp": "2022-06-30T11:43:41Z",
+          "url": "https://github.com/ringsaturn/tzf/pull/5/commits/1f82feefa9540e47b111496f02a7a5c45a92529e"
+        },
+        "date": 1657959834638,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetTimezoneName",
+            "value": 160115,
+            "unit": "ns/op",
+            "extra": "7281 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGetTimezoneNameAtEdge",
+            "value": 168068,
+            "unit": "ns/op",
+            "extra": "6913 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
