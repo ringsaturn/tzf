f05738522a5acac2035f71c2fa353d6b1e90565e
ringsaturn <ringsaturn.me@gmail.com>
2022-07-17 22:56:46 +0800

    add testcase

--- tzf_test.go
+++ tzf_test.go
@@ -2,7 +2,10 @@ package tzf_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+
+	"sort"
 
 	"github.com/ringsaturn/tzf"
 	tzfrel "github.com/ringsaturn/tzf-rel"
@@ -65,4 +68,41 @@ func ExampleFinder_GetTimezoneLoc() {
 	finder, _ := tzf.NewFinderFromPB(input)
 	fmt.Println(finder.GetTimezoneLoc(116.6386, 40.0786))
 	// Output: Asia/Shanghai <nil>
+}
+
+func ExampleFinder_GetTimezoneShapeByName() {
+	input := &pb.Timezones{}
+
+	// Lite data, about 16.7MB
+	dataFile := tzfrel.LiteData
+
+	if err := proto.Unmarshal(dataFile, input); err != nil {
+		panic(err)
+	}
+	finder, _ := tzf.NewFinderFromPB(input)
+	pbtz, err := finder.GetTimezoneShapeByName("Asia/Shanghai")
+	fmt.Printf("%v %v\n", pbtz.Name, err)
+	// Output: Asia/Shanghai <nil>
+}
+
+func ExampleFinder_GetTimezoneShapeByShift() {
+	input := &pb.Timezones{}
+
+	// Lite data, about 16.7MB
+	dataFile := tzfrel.LiteData
+
+	if err := proto.Unmarshal(dataFile, input); err != nil {
+		panic(err)
+	}
+	finder, _ := tzf.NewFinderFromPB(input)
+	pbtzs, _ := finder.GetTimezoneShapeByShift(28800)
+
+	pbnames := make([]string, 0)
+	for _, pbtz := range pbtzs {
+		pbnames = append(pbnames, pbtz.Name)
+	}
+	sort.Strings(pbnames)
+
+	fmt.Println(strings.Join(pbnames, ","))
+	// Output: Asia/Brunei,Asia/Choibalsan,Asia/Hong_Kong,Asia/Irkutsk,Asia/Kuala_Lumpur,Asia/Kuching,Asia/Macau,Asia/Makassar,Asia/Manila,Asia/Shanghai,Asia/Singapore,Asia/Taipei,Asia/Ulaanbaatar,Australia/Perth,Etc/GMT-8
 }
