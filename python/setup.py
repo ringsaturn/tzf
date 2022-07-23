54a24c2a4ae2cc1bc3886335c8c32893e9193b13
ringsaturn <ringsaturn.me@gmail.com>
2022-07-24 00:05:13 +0800

    debug

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,50 @@ import os
 
 from setuptools import setup
 
+import os
+import sysconfig
+
+from setuptools import setup
+from setuptools.command.build_py import build_py as _build_py
+
 version = os.popen("git describe --tags --always").read().split("-")[0]
+
+
+def get_ext_paths(root_dir, exclude_files):
+    """get filepaths for compilation"""
+    paths = []
+
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            if os.path.splitext(filename)[1] != ".py":
+                continue
+
+            file_path = os.path.join(root, filename)
+            if file_path in exclude_files:
+                continue
+
+            paths.append(file_path)
+    return paths
+
+
+# noinspection PyPep8Naming
+class build_py(_build_py):
+    def find_package_modules(self, package, package_dir):
+        ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+        modules = super().find_package_modules(package, package_dir)
+        filtered_modules = []
+        for (pkg, mod, filepath) in modules:
+            if os.path.exists(filepath.replace(".py", ext_suffix)):
+                continue
+            filtered_modules.append(
+                (
+                    pkg,
+                    mod,
+                    filepath,
+                )
+            )
+        return filtered_modules
+
 
 setup(
     name="tzfpy",
@@ -14,4 +57,5 @@ setup(
     packages=[""],
     package_dir={"": "."},
     package_data={"": ["tzfpy/tzf.so"]},
+    cmdclass={"build_py": build_py},
 )
