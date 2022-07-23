6e248049824b64bb9a8b3f8f4d29e4aca665724e
ringsaturn <ringsaturn.me@gmail.com>
2022-07-23 18:23:15 +0800

    chore: add cb for tzf-py

--- python/tzfpy/tests.py
+++ python/tzfpy/tests.py
@@ -8,5 +8,15 @@ class TestTZF(TestCase):
         self.assertEqual(get_tz(121.4737, 31.2305), "Asia/Shanghai")
 
 
+def _test_get_tz():
+    _ = get_tz(lng=13.358, lat=52.5061)
+    _ = get_tz(lng=116, lat=39)
+    _ = get_tz(lng=0.1276, lat=51.5073)
+
+
+def test_tz(benchmark):
+    benchmark(_test_get_tz)
+
+
 if __name__ == "__main__":
     main()
