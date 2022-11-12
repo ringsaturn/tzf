7f61eb6537f272d4bcc22ad1f5d6b0629be1fea8
ringsaturn <ringsaturn.me@gmail.com>
2022-11-12 21:44:17 +0800

    Fix memory leak in tzfpy (#63)
    - set timezone counts, names to Python vars, and never calling c_lib
    - add free func to free memory after got pointer from Go side, and send it back to Go side to free

--- python/tzfpy/__init__.py
+++ python/tzfpy/__init__.py
@@ -1,13 +1,21 @@
 import os
-from ctypes import CDLL, POINTER, c_char_p, c_float, c_long
+from ctypes import (
+    CDLL,
+    POINTER,
+    c_char_p,
+    c_float,
+    c_int,
+    c_long,
+    c_void_p,
+    string_at,
+)
 from typing import List
 
 c_lib = CDLL(os.path.join(os.path.dirname(__file__), "tzf.so"))
 
 _get = c_lib.GetTZ
 _get.argtypes = [POINTER(c_float), POINTER(c_float)]
-_get.restype = c_char_p
-
+_get.restype = c_void_p
 
 _names_counts = c_lib.CountTimezoneNames
 _names_counts.restype = c_long
@@ -15,21 +23,38 @@ _names_counts.restype = c_long
 _names = c_lib.TimezoneNames
 _names.restype = POINTER(c_char_p)
 
+# For free mem
+_free = c_lib.FreeChar
+_free.argtypes = [c_void_p]
+_free.restype = c_int
+
+_count = _names_counts()
+
+
+def _setup_timezone_names() -> List[str]:
+    res = _names()
+    names = []
+    for i in range(_count):
+        names.append(res[i].decode())
+    return names
+
+
+names = _setup_timezone_names()
+
 
 def get_tz(lng: float, lat: float) -> str:
-    return _get(c_float(lng), c_float(lat)).decode()
-
-
-def timezone_names_counts() -> int:
-    return _names_counts()
+    raw = _get(c_float(lng), c_float(lat))
+    ret = string_at(raw).decode()
+    _free(raw)
+    return ret
 
 
 def timezone_names() -> List[str]:
-    res = _names()
-    names = []
-    for i in range(timezone_names_counts()):
-        names.append(res[i].decode())
     return names
+
+
+def timezone_names_counts() -> int:
+    return _count
 
 
 if __name__ == "__main__":
