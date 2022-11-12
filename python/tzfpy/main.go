40e2c0bfcfa890d359018910fe5d4d0bc93a9ec9
ringsaturn <ringsaturn.me@gmail.com>
2022-11-12 21:37:53 +0800

    comments

--- python/tzfpy/main.go
+++ python/tzfpy/main.go
@@ -24,11 +24,15 @@ func init() {
 	timezoneCounts = len(finder.TimezoneNames())
 }
 
+// GetTZ for Python calling, please ensure calling [FreeChar] afer got value.
+//
 //export GetTZ
 func GetTZ(lng *C.float, lat *C.float) *C.char {
 	return C.CString(finder.GetTimezoneName(float64(*lng), float64(*lat)))
 }
 
+// FreeChar for other side to free *C.char which come free [GetTZ]
+//
 //export FreeChar
 func FreeChar(input unsafe.Pointer) {
 	C.free(input)
@@ -46,11 +50,15 @@ func goStringSliceToC(stringSlice []string) **C.char {
 	return (**C.char)(cArray)
 }
 
+// NOTE(ringsaturn): this func should only call once since no free mem method for return value.
+//
 //export TimezoneNames
 func TimezoneNames() **C.char {
 	return goStringSliceToC(finder.TimezoneNames())
 }
 
+// NOTE(ringsaturn): this func should only call once since no free mem method for return value.
+//
 //export CountTimezoneNames
 func CountTimezoneNames() C.long {
 	return C.long(timezoneCounts)
