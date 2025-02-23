25a9170fcde4346e45803f78587a7919d0cef516
Daniël van de Giessen <github@dvdgiessen.nl>
2025-02-23 15:25:50 +0100

    add option to lookup multiple coordinates from stdin (#184)

--- cmd/tzf/main.go
+++ cmd/tzf/main.go
@@ -2,8 +2,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/ringsaturn/tzf"
 	tzfrellite "github.com/ringsaturn/tzf-rel-lite"
@@ -26,12 +31,76 @@ func init() {
 	}
 }
 
+type StdinOrder int
+
+const (
+	LngLat StdinOrder = iota
+	LatLng
+)
+
 func main() {
 	var lng float64
 	var lat float64
-	flag.Float64Var(&lng, "lng", 116.3883, "longitude")
-	flag.Float64Var(&lat, "lat", 39.9289, "lontitude")
+	var stdinOrder StdinOrder
+	flag.Float64Var(&lng, "lng", 0.0, "Longitude")
+	flag.Float64Var(&lat, "lat", 0.0, "Latitude")
+	flag.Func("stdin-order", "Read multiple coordinates from stdin in given order", func(s string) error {
+		if s == "lng-lat" || s == "lon-lat" {
+			stdinOrder = LngLat
+		} else if s == "lat-lng" || s == "lat-lon" {
+			stdinOrder = LatLng
+		} else {
+			return errors.New("invalid order, must be one of lng-lat, lat-lng")
+		}
+		return nil
+	})
 	flag.Parse()
-
-	fmt.Println(finder.GetTimezoneName(lng, lat))
+	hasLng := false
+	hasLat := false
+	hasStdinOrder := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "lng" {
+			hasLng = true
+		}
+		if f.Name == "lat" {
+			hasLat = true
+		}
+		if f.Name == "stdin-order" {
+			hasStdinOrder = true
+		}
+	})
+	if hasLng != hasLat {
+		fmt.Fprintln(os.Stderr, "Both -lat and -lng must be passed")
+		os.Exit(2)
+	}
+	if (hasLng || hasLat) == hasStdinOrder {
+		fmt.Fprintln(os.Stderr, "Either -{lat/lng} or -stdin-order must be passed")
+		os.Exit(2)
+	}
+	if hasLng || hasLat {
+		fmt.Println(finder.GetTimezoneName(lng, lat))
+		return
+	}
+	if hasStdinOrder {
+		scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+		for scanner.Scan() {
+			line := strings.FieldsFunc(scanner.Text(), func(c rune) bool { return c == ' ' || c == '\t' || c == ',' || c == ';' })
+			if len(line) != 2 {
+				panic("Line does not contain two coordinates")
+			}
+			a, err := strconv.ParseFloat(line[0], 64)
+			if err != nil {
+				panic(err)
+			}
+			b, err := strconv.ParseFloat(line[1], 64)
+			if err != nil {
+				panic(err)
+			}
+			lng, lat := a, b
+			if stdinOrder == LatLng {
+				lng, lat = b, a
+			}
+			fmt.Println(finder.GetTimezoneName(lng, lat))
+		}
+	}
 }
